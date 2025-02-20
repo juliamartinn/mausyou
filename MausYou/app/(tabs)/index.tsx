1d7ecfefec02709b9bcdf99204f8d9ecab1e08c8
Mattes Kraus <mattes.kraus@uni-konstanz.de>
2025-02-20 20:58:44 +0100

    integrated expo push notifications. only incoming notifications work, can't send them or handle them in the app
    Co-authored-by: juliamartinn <juliamartinn@users.noreply.github.com>

--- MausYou/app/(tabs)/index.tsx
+++ MausYou/app/(tabs)/index.tsx
@@ -1,42 +1,122 @@
-import { Image, StyleSheet, Platform, Button, Alert } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+import { Image, StyleSheet, Alert } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import React from 'react';
-
-// push notifications
-import messaging from '@react-native-firebase/messaging';
-import {PermissionsAndroid} from 'react-native';
 
 
-export default function HomeScreen() {
-  const [mausname, setMausname] = React.useState('');
 
-  const sendMissRequest = async () => {
-    try {
-      const response = await fetch("http://185.250.249.46:3000/mausyou/add_mausyou", {
-        method: 'POST',  
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          requesterId: "2",
-          requestedId: "1",
-        }),
-      });
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
-      if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
-      const json = await response.json();
-      setMausname(json.name);
-      Alert.alert('Erfolgreich!', `MausYou Nachricht gesendet an ${json.name}`);
-    } catch (error) {
-      console.error('Network error:', error);
-      Alert.alert('Fehler!', 'Die Nachricht konnte nicht gesendet werden.');
-      setMausname("Network request failed");
-    }
+
+async function sendPushNotification(expoPushToken: string) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
   };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+
+function handleRegistrationError(errorMessage: string) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      handleRegistrationError('Permission not granted to get push token for push notification!');
+      return;
+    }
+    const projectId =
+      Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+    if (!projectId) {
+      handleRegistrationError('Project ID not found');
+    }
+    try {
+      const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e: unknown) {
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError('Must use physical device for push notifications');
+  }
+}
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState<Notifications.Notification | undefined>(
+    undefined
+  );
+  const notificationListener = useRef<Notifications.EventSubscription>();
+  const responseListener = useRef<Notifications.EventSubscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then(token => setExpoPushToken(token ?? ''))
+      .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(notificationListener.current);
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <ParallaxScrollView
@@ -50,16 +130,25 @@ export default function HomeScreen() {
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Sende eine MausYou Nachricht!</ThemedText>
         <HelloWave />
-        <ThemedText>{mausname ? `Gesendet an: ${mausname}` : ''}</ThemedText>
+        {/* <ThemedText>{mausname ? `Gesendet an: ${mausname}` : ''}</ThemedText> */}
       </ThemedView>
-
-      <ThemedView style={styles.stepContainer}>
-        <Button title="MausYou" onPress={sendMissRequest} />
-      </ThemedView>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+      <Text>Your Expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Title: {notification && notification.request.content.title} </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+      </View>
+      <Button
+        title="Press to Send Notification"
+        onPress={async () => {
+          await sendPushNotification(expoPushToken);
+        }}
+      />
+    </View>
     </ParallaxScrollView>
   );
 }
-
 
 const styles = StyleSheet.create({
   titleContainer: {
@@ -79,3 +168,59 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
 });
+
+// export default function HomeScreen() {
+//   // const [mausname, setMausname] = useState('');
+
+//   // const sendMissRequest = async () => {
+//   //   try {
+//   //     const response = await fetch("http://185.250.249.46:3000/mausyou/add_mausyou", {
+//   //       method: 'POST',  
+//   //       headers: {
+//   //         'Content-Type': 'application/json',
+//   //       },
+//   //       body: JSON.stringify({
+//   //         requesterId: "2",
+//   //         requestedId: "1",
+//   //       }),
+//   //     });
+
+//   //     if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+
+//   //     const json = await response.json();
+//   //     setMausname(json.name);
+//   //     Alert.alert('Erfolgreich!', `MausYou Nachricht gesendet an ${json.name}`);
+//   //   } catch (error) {
+//   //     console.error('Network error:', error);
+//   //     Alert.alert('Fehler!', 'Die Nachricht konnte nicht gesendet werden.');
+//   //     setMausname("Network request failed");
+//   //   }
+//   // };
+//
+//   return (
+//     <ParallaxScrollView
+//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+//       headerImage={
+//         <Image
+//           source={require('@/assets/images/mausis.png')}
+//           style={{ width: 500, height: 300 }}
+//         />
+//       }>
+//       {/* <ThemedView style={styles.titleContainer}>
+//         <ThemedText type="title">Sende eine MausYou Nachricht!</ThemedText>
+//         <HelloWave />
+//         <ThemedText>{mausname ? `Gesendet an: ${mausname}` : ''}</ThemedText>
+//       </ThemedView>
+
+//       <ThemedView style={styles.stepContainer}>
+//         <Button title="MausYou" onPress={sendMissRequest} />
+//       </ThemedView> */}
+//       <Text>Your Expo push token: {expoPushToken}</Text>
+//       <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Title: {notification && notification.request.content.title} </Text>
+//         <Text>Body: {notification && notification.request.content.body}</Text>
+//         <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+//       </View>
+//     </ParallaxScrollView>
+//   );
+// }
